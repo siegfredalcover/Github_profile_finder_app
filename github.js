57729ec9ec81f0ba7113f978a187b44a59c66f63
siegfredalcover <siegfred.work@gmail.com>
2021-05-04 01:49:52 +0800

    fixed github.js PATs

--- github.js
+++ github.js
@@ -1,31 +1,15 @@
 class Github {
-    constructor() {
-      this.config = {
-        headers: {
-          Authorization: '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'
-        }
-      }
-      this.repos_count = 5
-      this.repos_sort = 'created: asc'
-    }
-    async getUser(user) {
-      const profileResponse = await fetch(
-        `https://api.github.com/users/${user}`,
-        this.config
-      )
-   
-      const repoResponse = await fetch(
-        `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}`,
-        this.config
-      )
-   
-      const profile = await profileResponse.json()
-   
-      const repos = await repoResponse.json()
-   
-      return {
-        profile,
-        repos
-      }
+  constructor() {
+    this.authorization = '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';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?authorization=${this.authorization}`);
+
+    const profile = await profileResponse.json();
+
+    return {
+      profile
     }
   }
+}
